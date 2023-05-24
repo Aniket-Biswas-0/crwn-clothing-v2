bb0e5470c166adb76ef57de0988719b326e84965
Aniket Biswas <aniketbiswas88@gmail.com>
2023-05-24 15:05:50 +0530

    a

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyAhF2VESlKw02l4PPjB3btdtN9mhjjA9rE",
+  authDomain: "crwn-clothing-f3f4a.firebaseapp.com",
+  projectId: "crwn-clothing-f3f4a",
+  storageBucket: "crwn-clothing-f3f4a.appspot.com",
+  messagingSenderId: "995114780298",
+  appId: "1:995114780298:web:d070fde2e81a75817f8f73"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
